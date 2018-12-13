e02e2cda8ec7169b503d8e744b1bd23a38c60748
Tiago Martinho <t.martinho@icloud.com>
2018-12-13 12:38:20 +0100

    Define Child view controller as a UITableViewController

--- ContainerViewControllers/ChildViewController.swift
+++ ContainerViewControllers/ChildViewController.swift
@@ -1,8 +1,21 @@
 import UIKit
 
-class ChildViewController: UIViewController {
+class ChildViewController: UITableViewController {
+
+    private let cellReuseIdentifier = "cellReuseIdentifier"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)!
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
     }
 }
